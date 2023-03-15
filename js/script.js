951a8af9a71664b8cc626e04e915c6e7ee47c72f
geberi <126200667+geberich@users.noreply.github.com>
2023-03-16 00:13:02 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,45 @@
+"use strict";
+
+;
+
+(function () {
+  // TIME REMAIN
+  function getTimeRemaining(endtime) {
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor(t / 1000 % 60);
+    var minutes = Math.floor(t / 1000 / 60 % 60);
+    var hours = Math.floor(t / (1000 * 60 * 60) % 24);
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return {
+      total: t,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds
+    };
+  }
+
+  function initializeClock(id, endtime) {
+    function updateClock() {
+      var t = getTimeRemaining(endtime);
+
+      if (t.total <= 0) {
+        clearInterval(timeinterval);
+        var deadline = new Date(Date.parse(new Date()) + 13500 * 1000);
+        initializeClock('clockdiv', deadline);
+      }
+
+      var clock = document.querySelectorAll('.time-remain').forEach(function (item) {
+        item.querySelector(".hour").innerHTML = ("0" + t.hours).slice(-2);
+        item.querySelector(".minutes").innerHTML = ("0" + t.minutes).slice(-2);
+        item.querySelector(".seconds").innerHTML = ("0" + t.seconds).slice(-2);
+      });
+    }
+
+    updateClock();
+    var timeinterval = setInterval(updateClock, 1000);
+  }
+
+  var deadline = new Date(Date.parse(new Date()) + 13500 * 1000);
+  initializeClock("clockdiv", deadline);
+})();
